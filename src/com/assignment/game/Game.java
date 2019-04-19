3fb732e4292cfd31a8549ca172122c3ce854adf3
nurakmaljalil91 <nurakmaljalil91@gmail.com>
2019-04-19 17:12:44 +0800

    basic game window

--- src/com/assignment/game/Game.java
+++ src/com/assignment/game/Game.java
@@ -14,21 +14,31 @@ import java.util.Random;
  * @since 2019-04-19
  */
 
+ /**
+  * class game is inherited Canvas class properties 
+  * implements the runnable where the game can start,run and stop
+  */
 public class Game extends Canvas implements Runnable {
 
-    public static final int WIDTH = 448;
-    public static final int HEIGHT = 424;
-    public static final int TILESIZE = 64;
-    private Thread thread;
-    public boolean isRunning = false;
+    public static final int WIDTH = 448; // width for the window screen
+    public static final int HEIGHT = 424; // height for the window screen
+    public static final int TILESIZE = 64; // tileseze size for the tile in this case 64 * 64
+    private Thread thread; // thread of the program
+    public boolean isRunning = false; // check if the program is running
 
     // private MouseHandler mouseHandler;
-
+    /**
+     * Constructor for the game
+     */
     public Game() {
+        // Create window for the game
         Window window = new Window("Assignment 2019", WIDTH, HEIGHT, this);
 
     }
 
+    /**
+     * start the game start the thread
+     */
     public synchronized void start() {
         thread = new Thread(this);
         thread.start();
@@ -36,6 +46,9 @@ public class Game extends Canvas implements Runnable {
 
     }
 
+    /**
+     * stop the game and clean the thread
+     */
     public synchronized void stop() {
         try {
             thread.join();
@@ -47,27 +60,33 @@ public class Game extends Canvas implements Runnable {
         }
     }
 
+    /**
+     * run the game 
+     * calculate the fps of the game
+     * main loop of the game
+     */
     public void run() {
-        this.requestFocus();
-        long lastTime = System.nanoTime();
-        double framePerSeconds = 60.0;
-        double nanoSeconds = 1000000000 / framePerSeconds;
-        double delta = 0;
-        long timer = System.currentTimeMillis();
-        int frames = 0;
+        this.requestFocus(); // allow the game to be focus when start
+        long lastTime = System.nanoTime(); // check the last time for the game
+        double framePerSeconds = 60.0; // fps of the game
+        double nanoSeconds = 1000000000 / framePerSeconds; // calculate the fps in ns
+        double delta = 0; // delat time
+        long timer = System.currentTimeMillis(); // start the timer
+        int frames = 0; // init the frames
+        // main loop of the game
         while (isRunning) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / nanoSeconds;
-            lastTime = now;
+            long now = System.nanoTime(); // time for now
+            delta += (now - lastTime) / nanoSeconds; // delta time is time now divide by ns
+            lastTime = now; // init back the last time record 
             while (delta >= 1) {
-                update();
+                update(); // update all game
                 delta--;
             }
             if (isRunning) {
-                draw();
+                draw(); // render object on the window
 
             }
-            frames++;
+            frames++; // increase the frames
             if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
                 frames = 0;
@@ -76,13 +95,24 @@ public class Game extends Canvas implements Runnable {
         stop();
     }
 
+    /**
+     * Initialize the game
+     */
     public void initialize() {
         // mouseHandler = new MouseHandler();
     }
 
+    /**
+     * Update the game object inside the game
+     */
     public void update() {
 
     }
+
+    /**
+     * Draw the grid for the pieces
+     * @param g
+     */
     public void drawGrids(Graphics g){
         g.setColor(Color.black);
         for(int x = 0 ; x < WIDTH; x+=TILESIZE){
@@ -92,6 +122,10 @@ public class Game extends Canvas implements Runnable {
             g.drawLine(0, y, WIDTH, y);
         }
     }
+
+    /**
+     * draw all the ojects on the window of the game
+     */
     public void draw() {
         BufferStrategy bs = this.getBufferStrategy();
         if (bs == null) {
@@ -101,9 +135,9 @@ public class Game extends Canvas implements Runnable {
 
         Graphics g = bs.getDrawGraphics();
 
-        g.setColor(new Color(66, 134, 244));
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-        drawGrids(g);
+        g.setColor(new Color(66, 134, 244)); // color for the background
+        g.fillRect(0, 0, WIDTH, HEIGHT); // fill the background
+        drawGrids(g); // draw the grid
         /*
          * drawGrid(g); if(player != null){ player.draw(g); } else{
          * System.err.println("Player is null???"); }
@@ -111,10 +145,14 @@ public class Game extends Canvas implements Runnable {
          * debug.draw(g);
          */
         // gsm.render(g);
-        g.dispose();
-        bs.show();
+        g.dispose(); // clear the window screen
+        bs.show(); // show back the buffersrategy
     }
 
+    /**
+     * main function for running the game
+     * @param args
+     */
     public static void main(String[] args) {
         new Game();
     }
---
+++ src/com/assignment/game/states/GameState.java
@@ -0,0 +1,13 @@
+public abstract class GameState {
+    protected GameStateManager gameStateManager;
+
+    public GameState(GameStateManager gameStateManager) {
+        this.gameStateManager = gameStateManager;
+    }
+
+    public abstract void start();
+    public abstract void update(double delatTime);
+    public abstract void input(MouseHandler mouseHandler);
+    public abstract void draw(Window w);
+
+}
